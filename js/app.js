0f9a44793567b820dfab97993640f3983a92630a
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-06 23:33:31 -0700

    add player class structure

--- js/app.js
+++ js/app.js
@@ -16,8 +16,20 @@ class Player {
         this.totalGoods = 0;
         this.goods = {
             wood = [1, 3, 6, 10, 15, 21, 28, 36],
-
+            Stone: [2, 6, 12, 20, 30, 42, 56,],
+            Pottery: [3, 9, 18, 30, 45, 63],
+            Textiles: [4, 12, 24, 40, 60],
+            Weapons: [5, 15, 30, 50]
         };
     }
+    dicePool(){
+        console.log(`player rolls ${this.citiesBuilt} dice`)
+    }
+    goodsCollected(){
+        console.log(`player collects goods`)
+    }
+    Calculate Food
+    Calculate Disaster Score
+    Calculate Score
 
 };
