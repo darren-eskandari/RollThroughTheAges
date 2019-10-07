f978dddd0173bdd50d5de3dca2e7116201680c55
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-06 23:26:58 -0700

    add player class

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// player class
+class Player {
+    constructor(){
+        this.name = name;
+        this.isStartPlayer = true;
+        this.citiesBuilt = 3;
+        this.monumentsBuilt = [];
+        this.developmentsCompleted = [];
+        this.food = 3;
+        this.availableWorkers = 0;
+        this.monumentScore = 0;
+        this.developmentScore = 0;
+        this.bonusScore = 0;
+        this.disasterScore = 0;
+        this.totalScore = 0;
+        this.totalGoods = 0;
+        this.goods = {
+            wood = [1, 3, 6, 10, 15, 21, 28, 36],
+
+        };
+    }
+
+};
