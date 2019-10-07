623240bd8bc0c6d3968187f2bcbd0ad51a1b9e77
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-07 15:31:44 -0700

    game object outlined

--- js/app.js
+++ js/app.js
@@ -5,35 +5,94 @@ class Player {
         this.isStartPlayer = true;
         this.citiesBuilt = 3;
         this.monumentsBuilt = [];
-        this.developmentsCompleted = [];
+        // this.developmentsCompleted = [];
         this.food = 3;
         this.availableWorkers = 0;
-        this.totalGoods = 0;
-        this.goods = {
-            wood: [1, 3, 6, 10, 15, 21, 28, 36],
-            stone: [2, 6, 12, 20, 30, 42, 56,],
-            pottery: [3, 9, 18, 30, 45, 63],
-            textiles: [4, 12, 24, 40, 60],
-            weapons: [5, 15, 30, 50]
-        };
+        // this.totalGoods = 0;
+        // this.goods = {
+        //     wood: [1, 3, 6, 10, 15, 21, 28, 36],
+        //     stone: [2, 6, 12, 20, 30, 42, 56,],
+        //     pottery: [3, 9, 18, 30, 45, 63],
+        //     textiles: [4, 12, 24, 40, 60],
+        //     weapons: [5, 15, 30, 50]
+        // };
         this.score = {
             monuments: 0,
-            developments: 0,
-            bonus: 0, 
+            // developments: 0,
+            // bonus: 0, 
             disaster: 0,
             total: 0
         };
     }
 
-    calculateGoods(){
-        // console.log(`player collects goods`)
+    works =  {
+        city4: {
+            progress: 0,
+            complete: 3,
+            completed: false,
+        },
+        city5: {
+            progress: 0,
+            complete: 4,
+            completed: false,
+        },
+        city6: {
+            progress: 0,
+            complete: 5,
+            completed: false,
+        },
+        city7: {
+            progress: 0,
+            complete: 6,
+            completed: false,
+        },
+        stepPyramid: {
+            progress: 0,
+            complete: 3,
+            completed: false,
+        },
+        stoneCircle: {
+            progress: 0,
+            complete: 5,
+            completed: false,
+        },
+        temple: {
+            progress: 0,
+            complete: 7,
+            completed: false,
+        },
+        obelisk: {
+            progress: 0,
+            complete: 9,
+            completed: false,
+        },
+        hangingGarden: {
+            progress: 0,
+            complete: 11,
+            completed: false,
+        },
+        greatWall: {
+            progress: 0,
+            complete: 13,
+            completed: false,
+        },
+        greatPyramid: {
+            progress: 0,
+            complete: 15,
+            completed: false,
+        }
     }
+
+
+    // calculateGoods(){
+    //     // console.log(`player collects goods`)
+    // }
     calculateFood(){
         this.food = this.food + game.foodRolled - this.citiesBuilt;
          
     }
     calculateWorkers(){
-        this.availableWorkers = this.availableWorkers + game.workersRolled;
+        this.availableWorkers = game.workersRolled;
          
     }
     calculateDisaster(){
@@ -42,9 +101,9 @@ class Player {
     calculateScore(){
     
     }
-    developmentModifiers(){
-    
-    }
+    // developmentModifiers(){
+    // }
+
 };
 
 const players = [];
@@ -64,11 +123,11 @@ const game = {
     },
 
     // temp values space holder waiting for random dice roll functions
-    foodRolled: 5,
-    workersRolled: 3,
-    goodsRolled: 2,
-    coinsRolled: 7,
-    disastersRolled: 1,
+        foodRolled: 5,
+        workersRolled: 3,
+        goodsRolled: 2,
+        coinsRolled: 7,
+        disastersRolled: 1,
 
     //collect food function
     assignResults(){
@@ -81,46 +140,33 @@ const game = {
 
     },
 
-    works: {
-        stepPyramid: {
-            progress: 0,
-            complete: 3,
-        },
-        stoneCircle: {
-            progress: 0,
-            complete: 5,
-        },
-        temple: {
-            progress: 0,
-            complete: 7,
-        },
-        obelisk: {
-            progress: 0,
-            complete: 9,
-        },
-        hangingGarden: {
-            progress: 0,
-            complete: 11,
-        },
-        greatWall: {
-            progress: 0,
-            complete: 13,
-        },
-        greatPyramid: {
-            progress: 0,
-            complete: 15,
-        }
-    },
-
     // build works by assigning available workers
     buildWorks(){
-
+        // assign workers from available pool to works and check for completion
     },
     
+    // sell goods and use coins to build developments
+    purchaseDevelopments(){
+        // allow player to sell goods, add coins, and purchase a single development
+        // call the discard goods function when purchase confirmed ** optional
+    },
+
+    // discard goods in excess of 6 ** optional
+    discardGoods(){
+        // prompt the player to discard 
+        // confirmation calls the end of turn
+    },
+
+    // all functions that occur at end of turn
+    endTurn(){
+        // calls score calculation functions and checks for end of game triggers.
+    },
+
 };
 
  
 $('#start').on('click', () => {
+    // when adding additional player functionality, create a conditional that limits maximum number of players
     game.newPlayer();
 });
 
