264ae77a249286c3a2e3012cd4f8ac5b34e9b967
GA SEI <GA-SEI@Darrens-MacBook-Pro-2.local>
2019-10-09 01:06:41 -0700

    images and buttons added

--- js/app.js
+++ js/app.js
@@ -5,9 +5,16 @@ class Player {
         this.isStartPlayer = true;
         this.citiesBuilt = 3;
         this.monumentsBuilt = [];
-        // this.developmentsCompleted = [];
         this.food = 3;
         this.availableWorkers = 0;
+        this.score = {
+            monuments: 0,
+            disaster: 0,
+            total: 0
+            // developments: 0,
+            // bonus: 0, 
+        };
+        // this.developmentsCompleted = [];
         // this.totalGoods = 0;
         // this.goods = {
         //     wood: [1, 3, 6, 10, 15, 21, 28, 36],
@@ -16,113 +23,139 @@ class Player {
         //     textiles: [4, 12, 24, 40, 60],
         //     weapons: [5, 15, 30, 50]
         // };
-        this.score = {
-            monuments: 0,
-            // developments: 0,
-            // bonus: 0, 
-            disaster: 0,
-            total: 0
-        };
     }
 
     works =  {
+        city1: {
+            // progress: 0,
+            // complete: 0,
+            completed: true,
+            score: 0,
+            image: 'images/city1.png',
+        },
+        city2: {
+            // progress: 0,
+            // complete: 0,
+            completed: true,
+            score: 0,
+            image: 'images/city2.png',
+        },
+        city3: {
+            // progress: 0,
+            // complete: 0,
+            completed: true,
+            score: 0,
+            image: 'images/city3.png',
+        },
         city4: {
             progress: 0,
             complete: 3,
             completed: false,
+            score: 0,
+            image: 'images/city4.png',
         },
         city5: {
             progress: 0,
             complete: 4,
             completed: false,
+            score: 0,
+            image: 'images/city5.png',
         },
         city6: {
             progress: 0,
             complete: 5,
             completed: false,
+            score: 0,
+            image: 'images/city6.png',
         },
         city7: {
             progress: 0,
             complete: 6,
             completed: false,
+            score: 0,
+            image: 'images/city7.png',
         },
         stepPyramid: {
             progress: 0,
             complete: 3,
             completed: false,
             score: 1,
+            image: 'images/step_pyramid.png',
         },
         stoneCircle: {
             progress: 0,
             complete: 5,
             completed: false,
             score: 2,
+            image: 'images/stone_circle.png',
         },
         temple: {
             progress: 0,
             complete: 7,
             completed: false,
             score: 4,
+            image: 'images/temple.png',
         },
         obelisk: {
             progress: 0,
             complete: 9,
             completed: false,
             score: 6,
+            image: 'images/obelisk.png',
         },
         hangingGarden: {
             progress: 0,
             complete: 11,
             completed: false,
             score: 8,
+            image: 'images/hanging_garden.png',
         },
         greatWall: {
             progress: 0,
             complete: 13,
             completed: false,
             score: 10,
+            image: 'images/great_wall.png',
         },
         greatPyramid: {
             progress: 0,
             complete: 15,
             completed: false,
             score: 12,
+            image: 'images/great_pyramid.png',
         }
     }
-
-
-    // calculateGoods(){
-    //     // console.log(`player collects goods`)
-    // }
+    
+    calculateDisaster(){
+        if (game.val.disasters === 1){
+            // no results on 1 disaster
+        } else if(game.val.disasters === 3){
+            // results effect other players on 3 disasters
+        } else {
+            this.score.disaster -= game.val.disasters;
+        }
+    }
     calculateFood(){
-        this.food = this.food + game.foodRolled - this.citiesBuilt;
+        this.food = this.food + game.val.food - this.citiesBuilt;
         if (this.food < 0){
             this.score.disaster += this.food;
             this.food = 0;
         }
-         
     }
     calculateWorkers(){
-        this.availableWorkers = game.workersRolled;
-        
-    }
-    calculateDisaster(){
-        if (game.disastersRolled === 1){
-            // no results
-        } else if(game.disastersRolled === 3){
-            // results effect other players
-        } else {
-            this.score.disaster -= game.disastersRolled;
-        }
+        this.availableWorkers = game.val.workers;
     }
     calculateScore(){
-    
+        
     }
-    // developmentModifiers(){
+    // calculateGoods(){
+    //  console.log(`player collects goods`)
     // }
-
+    // developmentModifiers(){
+    //    
+    // }
 };
-
+    
 const players = [];
 // player 1 to be replaced with current player variable later
 
@@ -132,7 +165,8 @@ const game = {
         setName = prompt('Name your civilization');
         const player1 = new Player(setName);
         players.push(player1)
-        // console.log(player1);
+        $('#rollDice').text(`Roll Dice`)
+        this.render();
     },
 
     diceResults: 
@@ -275,34 +309,35 @@ const game = {
     },
 
     // rolled values
-        foodRolled: 0,
-        workersRolled: 0,
-        // goodsRolled: 0,
-        // coinsRolled: 0,
-        disastersRolled: 0,
+    val: {
+        disasters: 0,
+        food: 0,
+        workers: 0,
+        // goods: 0,
+        // coins: 0,
+    },
 
     //collect food function
     assignResults(){
         for (let i = 0; i < this.finalResult.length; i++){
             if (this.finalResult[i].result === 'disaster'){
-                this.disastersRolled = this.disastersRolled + this.finalResult[i].amount;
+                this.val.disasters = this.val.disasters + this.finalResult[i].amount;
             } else if (this.finalResult[i].result === 'food'){
-                this.foodRolled = this.foodRolled + this.finalResult[i].amount;
+                this.val.food = this.val.food + this.finalResult[i].amount;
             } else if (this.finalResult[i].result === 'worker'){
-                this.workersRolled = this.workersRolled + this.finalResult[i].amount;
+                this.val.workers = this.val.workers + this.finalResult[i].amount;
             } else if (this.finalResult[i].result === 'both'){
-                this.workersRolled = this.workersRolled + this.finalResult[i].amount;
-                this.foodRolled = this.foodRolled + this.finalResult[i].amount;
+                this.val.workers = this.val.workers + this.finalResult[i].amount;
+                this.val.food = this.val.food + this.finalResult[i].amount;
             }
         }
 
-        // players[0].calculateGoods();
         players[0].calculateFood();
         players[0].calculateWorkers();
         players[0].calculateDisaster();
-        // console.log(players);
-        // console.log(players[0]);
+        // players[0].calculateGoods();
 
+        this.render();
     },
 
     // build works by assigning available workers
@@ -311,26 +346,49 @@ const game = {
 
     },
     
-    // sell goods and use coins to build developments
-    purchaseDevelopments(){
-        // allow player to sell goods, add coins, and purchase a single development
-        // call the discard goods function when purchase confirmed ** optional
-    },
+    // // sell goods and use coins to build developments
+    // purchaseDevelopments(){
+    //     // allow player to sell goods, add coins, and purchase a single development
+    //     // call the discard goods function when purchase confirmed ** optional
+    // },
 
-    // discard goods in excess of 6 ** optional
-    discardGoods(){
-        // prompt the player to discard 
-        // confirmation calls the end of turn
-    },
+    // // discard goods in excess of 6 ** optional
+    // discardGoods(){
+    //     // prompt the player to discard 
+    //     // confirmation calls the end of turn
+    // },
 
     // all functions that occur at end of turn
     endTurn(){
         // calls score calculation functions and checks for end of game triggers.
     },
 
-};
+    // render game state
+    render(){
+        $('#name').text(`Player: ${players[0].name}`);
+        $('#food').text(`Food: ${players[0].food}`);
+        $('#workers').text(`Available Workers: ${players[0].availableWorkers}`);
+        
+        $('#city1').html(`<img src="${players[0].works.city1.image}">`);
+        $('#city2').html(`<img src="${players[0].works.city2.image}">`);
+        $('#city3').html(`<img src="${players[0].works.city3.image}">`);
+        $('#city4').html(`<img src="${players[0].works.city4.image}"> ${players[0].works.city4.progress}/${players[0].works.city4.complete}`);
+        $('#city5').html(`<img src="${players[0].works.city5.image}"> ${players[0].works.city5.progress}/${players[0].works.city5.complete}`);
+        $('#city6').html(`<img src="${players[0].works.city6.image}"> ${players[0].works.city6.progress}/${players[0].works.city6.complete}`);
+        $('#city7').html(`<img src="${players[0].works.city7.image}"> ${players[0].works.city7.progress}/${players[0].works.city7.complete}`);
+        
+        $('#stepPyramid').html(`<img src="${players[0].works.stepPyramid.image}"> ${players[0].works.stepPyramid.progress}/${players[0].works.stepPyramid.complete}`)
+        $('#stoneCircle').html(`<img src="${players[0].works.stoneCircle.image}"> ${players[0].works.stoneCircle.progress}/${players[0].works.stoneCircle.complete}`)
+        $('#temple').html(`<img src="${players[0].works.temple.image}"> ${players[0].works.temple.progress}/${players[0].works.temple.complete}`)
+        $('#obelisk').html(`<img src="${players[0].works.obelisk.image}"> ${players[0].works.obelisk.progress}/${players[0].works.obelisk.complete}`)
+        $('#hangingGarden').html(`<img src="${players[0].works.hangingGarden.image}"> ${players[0].works.hangingGarden.progress}/${players[0].works.hangingGarden.complete}`)
+        $('#greatWall').html(`<img src="${players[0].works.greatWall.image}"> ${players[0].works.greatWall.progress}/${players[0].works.greatWall.complete}`)
+        $('#greatPyramid').html(`<img src="${players[0].works.greatPyramid.image}"> ${players[0].works.greatPyramid.progress}/${players[0].works.greatPyramid.complete}`)
 
+    },
+};
  
+// call the start of game function
 $('#start').on('click', () => {
     // when adding additional player functionality, create a conditional that limits maximum number of players
     game.newPlayer();
